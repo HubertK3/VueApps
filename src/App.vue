218dcb00422f4578da8e74f94b8dc8fdcf714d09
HubertK3 <hubertchal1377@gmail.com>
2023-11-20 21:09:43 +0100

    zmiany

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,56 @@
 <template>
+  <div class="upper-container">
+    <div class="timer">{{ timeLeft }}</div>
+    <Custom-Button></Custom-Button>
+  </div>
   <div id="app" class="page">
     <div class="calculator">
       <div class="result-window">{{ resultWindow }}</div>
       <div class="display">{{ display }}</div>
       <div class="buttons">
-        <button @click="appendToDisplay('1')">1</button>
-        <button @click="appendToDisplay('2')">2</button>
-        <button @click="appendToDisplay('3')">3</button>
-        <button @click="appendToDisplay('+')">+</button>
-        <button @click="appendToDisplay('4')">4</button>
-        <button @click="appendToDisplay('5')">5</button>
-        <button @click="appendToDisplay('6')">6</button>
-        <button @click="appendToDisplay('-')">-</button>
-        <button @click="appendToDisplay('7')">7</button>
-        <button @click="appendToDisplay('8')">8</button>
-        <button @click="appendToDisplay('9')">9</button>
-        <button @click="appendToDisplay('*')">*</button>
-        <button @click="appendToDisplay('0')">0</button>
-        <button @click="appendToDisplay('**')">^</button>
-        <button @click="removeLastDigit">Back</button>
+        <button v-for="button in buttons" :key="button" @click="handleButtonClick(button)">{{ button }}</button>
         <button @click="clearDisplay">C</button>
-        <button @click="calculateResult">=</button>
-        <button @click="appendToDisplay('/')">/</button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import CustomButton from './CustomButton.vue';
+
 export default {
+  components: {
+    CustomButton,
+  },
   data() {
     return {
       display: "",
       resultWindow: "",
+      buttons: ['1', '2', '3', '+', '4', '5', '6', '-', '7', '8', '9', '*', '0', '%', '/', '='],
+      timer: null,
+      timeLeft: '10:00',
     };
   },
+  mounted() {
+    this.startTimer();
+  },
   methods: {
+    handleButtonClick(button) {
+      if (button === '=') {
+        this.calculateResult();
+      } else if (button === '%') {
+        this.calculatePercentage();
+      } else {
+        this.appendToDisplay(button);
+      }
+    },
     appendToDisplay(value) {
       this.display += value;
     },
-    removeLastDigit() {
-      this.display = this.display.slice(0, -1);
+    calculatePercentage() {
+      if (this.display !== "") {
+        this.display = String(parseFloat(this.display) / 100);
+      }
     },
     clearDisplay() {
       this.display = "";
@@ -48,16 +58,36 @@ export default {
     },
     calculateResult() {
       try {
-        const result = eval(this.display);
+        const result = new Function('return ' + this.display)();
         if (result === Infinity || isNaN(result)) {
           this.resultWindow = "Nie dziel przez 0";
         } else {
           this.resultWindow = this.display + " = " + result;
-          this.display = result;
+          this.display = result.toString();
         }
       } catch (error) {
         this.resultWindow = "Błąd";
       }
+    },
+    startTimer() {
+      let time = 600;
+      this.timer = setInterval(() => {
+        if (time > 0) {
+          time--;
+          this.timeLeft = `${Math.floor(time / 60)}:${('0' + time % 60).slice(-2)}`;
+        } else {
+          clearInterval(this.timer);
+          if (confirm('Czas upłynął! Czy zresetować timer?')) {
+            this.resetTimer();
+          }
+        }
+      }, 1000);
+    },
+    resetTimer() {
+      clearInterval(this.timer);
+      this.timer = null;
+      this.timeLeft = '10:00';
+      this.startTimer();
     },
   },
 };
@@ -79,17 +109,23 @@ export default {
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
   width: 300px;
 }
-
 .display {
   font-size: 24px;
   margin-bottom: 10px;
 }
-
 .buttons button {
   font-size: 18px;
   width: 60px;
   height: 60px;
   margin: 5px;
+}
+.timer {
+  font-size: 20px;
+  color: rgb(21, 21, 21);
+}
+.upper-container {
+  text-align: left;
+  margin-bottom: 0px;
 }
 </style>
 
---
+++ src/CustomButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <button @click="showMessage">Kliknij</button>
+</template>
+
+<script>
+export default {
+  methods: {
+    showMessage() {
+      alert('Nic nie robie :D');
+    },
+  },
+};
+</script>
